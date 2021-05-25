5bb7d4245f8a4686901d28eb06d30953382de18f
Егор Тихонов <gregor18rus@gmail.com>
2021-05-25 16:13:26 +0400

    StringExtensions

---
+++ BokuNoGame/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BokuNoGame.Extensions
+{
+    public static class StringExtensions
+    {
+        public static HtmlString ToHtmlString(this string value)
+        {
+            return new HtmlString(value);
+        }
+    }
+}
