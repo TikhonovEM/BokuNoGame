6decf2d80a934ecb3a729b5f4a55f5e58c855684
Егор Тихонов <gregor18rus@gmail.com>
2021-06-01 19:50:47 +0400

    строковый ид в II

--- BokuNoGame/Models/IntegrationInfo.cs
+++ BokuNoGame/Models/IntegrationInfo.cs
@@ -10,6 +10,7 @@ namespace BokuNoGame.Models
         public int Id { get; set; }
         public string ExternalSystemDescriptor { get; set; }
         public int ExternalGameId { get; set; }
+        public string ExternalGameIdStr { get; set; }
         public int InternalGameId { get; set; }
         public DateTime Date { get; set; }
     }
