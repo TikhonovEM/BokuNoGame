4a5d1a9424fc6929f2836caae8d5e3bce1e4aa19
Егор Тихонов <gregor18rus@gmail.com>
2021-06-01 19:45:51 +0400

    Класс для EGS интеграции

---
+++ BokuNoGame/IntegrationServices/EpicGamesIntegrationService.cs
@@ -0,0 +1,50 @@
+﻿using BokuNoGame.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BokuNoGame.IntegrationServices
+{
+    public class EpicGamesIntegrationService : IBaseIntegrationService
+    {
+        private readonly ILogger<SteamIntegratonService> _logger;
+        private readonly AppDBContext _appDBContext;
+        public EpicGamesIntegrationService(ILogger<SteamIntegratonService> logger, AppDBContext appDBContext)
+        {
+            _logger = logger;
+            _appDBContext = appDBContext;
+        }
+        public string ExternalSystemDescriptor { get; set; } = "Epic Games";
+
+        public async Task GetActualDataAsync()
+        {
+            var scriptFolder = Startup.Configuration["IntegrationSettings:EpicGamesIntegrationSettings:ScriptFolderPath"];
+            var script = Startup.Configuration["IntegrationSettings:EpicGamesIntegrationSettings:ScriptName"];
+
+            var pythonPath = Startup.Configuration["IntegrationSettings:EpicGamesIntegrationSettings:PythonPath"];
+
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(pythonPath, Path.Combine(scriptFolder, script))
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            p.Start();
+
+            var output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            Console.WriteLine(output);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _appDBContext.SaveChangesAsync();
+        }
+    }
+}
